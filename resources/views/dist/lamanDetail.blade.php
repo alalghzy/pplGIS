33d691a6d31b8b681f6839fe1f8f9149e5412f59
alalghzy <alalghzy@gmail.com>
2023-12-17 01:40:44 +0700

    O

--- resources/views/dist/lamanDetail.blade.php
+++ resources/views/dist/lamanDetail.blade.php
@@ -25,7 +25,7 @@
             <div class="col-12 col-lg-12">
                 <div class="row">
                     <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
+                        <div class="card shadow-sm">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
@@ -48,7 +48,7 @@
                         </div>
                     </div>
                     <div class="col-6 col-lg-2 col-md-6">
-                        <div class="card">
+                        <div class="card shadow-sm">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
@@ -88,7 +88,7 @@
                         </div>
                     </div>
                     <div class="col-6 col-lg-2 col-md-6">
-                        <div class="card">
+                        <div class="card shadow-sm">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
@@ -118,7 +118,7 @@
                         </div>
                     </div>
                     <div class="col-6 col-lg-2 col-md-6">
-                        <div class="card">
+                        <div class="card shadow-sm">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
@@ -151,7 +151,7 @@
                         </div>
                     </div>
                     <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
+                        <div class="card shadow-sm">
                             <div class="card-body px-4 py-4-5">
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
